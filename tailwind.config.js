259b50fa66b502ec4c38e6fa2558b4cfa60918d4
koch solach <solachthi@gmail.com>
2023-12-21 23:37:45 +0700

    confige screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "320px", // iPhone 5
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px", // iPhone 14 Pro Max
+      },
+    },
   },
   plugins: [],
 };
